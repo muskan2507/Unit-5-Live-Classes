402918318e41b21b88971e6ff0f793123043e255
Muskan Shaw <princesahil0065@gmail.com>
2022-05-20 12:11:59 +0530

    gg

--- Performance_optimization/src/Components/TodoItem.tsx
+++ Performance_optimization/src/Components/TodoItem.tsx
@@ -1,13 +1,20 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import { todoType } from './Todos'
 
 const TodoItem = (props:todoType) => {
+    const [status,setstaus]=useState<boolean>(props.status)
   return (
     <div>
-        {props.title}-{props.status?"Done":"Not done"}
+        {props.title}-{status?"Done":"Not done"}
+        <button onClick={()=>setstaus(!status)}>Toggle</button>
     </div>
   )
 }
 
 export default TodoItem
-export const MemoTodo=memo(TodoItem)
+export const MemoTodo=memo(TodoItem,function eq(prev,curr){
+    if(prev.id==curr.id){
+        return true;
+    }
+    return false;
+})
